6945aa78775b58dc73ff827c9e49382631b30d62
ductd10062003 <ductdhe176150@fpt.edu.vn>
2025-03-10 09:05:57 +0700

    update admin

--- PRN222-Final-Project/PRN222-Final-Project/Repositories/Interface/IUserRepository.cs
+++ PRN222-Final-Project/PRN222-Final-Project/Repositories/Interface/IUserRepository.cs
@@ -5,5 +5,9 @@ namespace PRN222_Final_Project.Repositories.Interface
     public interface IUserRepository : IGenericRepository<User>
     {
         Task<User> login(string email, string password);
+        Task<(IEnumerable<User> Items, int TotalCount)> GetUserPagesAsync(
+           string search = null,
+           int pageNumber = 1,
+           int pageSize = 10);
     }
 }
--- PRN222-Final-Project/PRN222-Final-Project/Services/Interface/IUserService.cs
+++ PRN222-Final-Project/PRN222-Final-Project/Services/Interface/IUserService.cs
@@ -5,5 +5,9 @@ namespace PRN222_Final_Project.Services.Interface
     public interface IUserService : IGenericService<User>
     {
         Task<User> Login(string username, string password);
+        Task<(IEnumerable<User> Items, int TotalCount)> GetUserPagesAsync(
+           string search = null,
+           int pageNumber = 1,
+           int pageSize = 10);
     }
 }
